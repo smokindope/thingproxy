01d62380f21464da9689802b940a764d3a34a3cb
smokindope <smokindopemcfc247@gmail.com>
2024-04-01 03:14:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.port = process.env.PORT || 3000;
 exports.enable_logging = false;
 exports.fetch_regex = /^\/fetch\/(.*)$/; // The URL to look for when parsing the request.
-exports.proxy_request_timeout_ms = 0; // The lenght of time we'll wait for a proxy server to respond before timing out.
-exports.max_request_length = 0; // The maximum length of characters allowed for a request or a response.
+exports.proxy_request_timeout_ms = 100000000000000000; // The lenght of time we'll wait for a proxy server to respond before timing out.
+exports.max_request_length = 10000000000000000; // The maximum length of characters allowed for a request or a response.
 exports.enable_rate_limiting = true;
 exports.max_requests_per_second = 100; // The maximum number of requests per second to allow from a given IP.
 exports.blacklist_hostname_regex = /^(10\.|192\.|127\.|localhost$)/i; // Good for limiting access to internal IP addresses and hosts.
